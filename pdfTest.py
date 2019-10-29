efaa88b690bcde72b3feccec1fa026023b64e39f
francesco.piccolo <francesco.piccolo@lynxspa.com>
2019-10-29 16:02:21 +0100

    Crea un file pdf vuoto, e ci mette un rettangolo nero

--- pdfTest.py
+++ pdfTest.py
@@ -12,7 +12,17 @@ filepath = os.path.join(dirpath, filename)
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.output(filepath)
 
-
-black_image_file = 'Black.png'
-blue_image_file = 'Blue.png'
-#doc = fitz.open(filename)
+black_image_file = dirpath+'/Black.png'
+blue_image_file = dirpath+'/Blue.png'
+#Aggiungo le immagini al file pdf
+#https://github.com/pymupdf/PyMuPDF/wiki/How-to-Insert-new-PDF-Pages,-Images-and-Text
+# define the position (upper-right corner)
+image_rectangle = fitz.Rect(450, 20, 550, 120)
+# retrieve the first page of the PDF
+file_handle = fitz.open(filepath)
+first_page = file_handle[0]
+# add the image
+pix = fitz.Pixmap(black_image_file)
+first_page.insertImage(image_rectangle, pixmap=pix, overlay=True)
+#Se cambio il nome dell'output funziona
+file_handle.save(filepath,incremental=True)
