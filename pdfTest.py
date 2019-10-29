d32d5e4fd79f2667fdc8c1b7b14efed23fcb89a6
francesco.piccolo <francesco.piccolo@lynxspa.com>
2019-10-29 17:43:21 +0100

    Crea un file pdf vuoto, e ci mette un rettangolo nero e uno blu

--- pdfTest.py
+++ pdfTest.py
@@ -2,7 +2,6 @@
 #Uso PyMuPDF https://pymupdf.readthedocs.io/en/latest/tutorial/
 import fitz
 import os
-from shutil import copyfile
 from fpdf import FPDF
 filename = 'blank.pdf'
 output_file='output.pdf'
@@ -17,6 +16,7 @@ black_image_file = dirpath+'/Black.png'
 blue_image_file = dirpath+'/Blue.png'
 #Aggiungo le immagini al file pdf
 #https://github.com/pymupdf/PyMuPDF/wiki/How-to-Insert-new-PDF-Pages,-Images-and-Text
+#https://stackabuse.com/working-with-pdfs-in-python-adding-images-and-watermarks/
 # define the position (upper-right corner)
 image_rectangle = fitz.Rect(450, 20, 550, 120)
 # retrieve the first page of the PDF
@@ -25,7 +25,8 @@ first_page = file_handle[0]
 # add the image
 #pix = fitz.Pixmap(black_image_file)
 #first_page.insertImage(image_rectangle, pixmap=pix, overlay=True)
+#https://pymupdf.readthedocs.io/en/latest/rect/
 first_page.insertImage(fitz.Rect(450, 20, 550, 120), pixmap=fitz.Pixmap(black_image_file), overlay=True)
-first_page.insertImage(fitz.Rect(250, 20, 550, 120), pixmap=fitz.Pixmap(blue_image_file), overlay=True)
+first_page.insertImage(fitz.Rect(50, 20, 350, 120), pixmap=fitz.Pixmap(blue_image_file), overlay=True)
 #Se cambio il nome dell'output funziona
 file_handle.save(output_filepath)
