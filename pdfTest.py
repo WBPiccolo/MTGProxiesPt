27092440fee855e7a3ba944396a1c38267d7adb2
francesco.piccolo <francesco.piccolo@lynxspa.com>
2019-10-29 14:40:17 +0100

    Crea un file pdf vuoto

--- pdfTest.py
+++ pdfTest.py
@@ -1,7 +1,18 @@
 #File di test per la creazione di pdf
-import PyPDF2
+#Uso PyMuPDF https://pymupdf.readthedocs.io/en/latest/tutorial/
+import fitz
+import os
+from shutil import copyfile
+from fpdf import FPDF
+filename = 'output.pdf'
+dirpath =os.path.dirname(__file__)
+filepath = os.path.join(dirpath, filename)
 
-output_file = 'output.pdf'
+# Creo il file pdf
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.output(filepath)
+
+
 black_image_file = 'Black.png'
 blue_image_file = 'Blue.png'
-
+#doc = fitz.open(filename)
